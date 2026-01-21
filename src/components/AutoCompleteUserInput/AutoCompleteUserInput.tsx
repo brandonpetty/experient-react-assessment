d7b052f1f48f76d951f47ff7de556a147d1498fa
Brandon Petty <brandon@petty.io>
2026-01-21 12:48:34 -0500

    fix minor formatting issue

--- src/components/AutoCompleteUserInput/AutoCompleteUserInput.tsx
+++ src/components/AutoCompleteUserInput/AutoCompleteUserInput.tsx
@@ -66,8 +66,8 @@ const UserAutocomplete: React.FC = () => {
     const fetchUsers = async () => {
       try {
         const response = await fetch(
-        "https://jsonplaceholder.typicode.com/users"
-      );
+          "https://jsonplaceholder.typicode.com/users"
+        );
         const data: User[] = await response.json();
         const formattedUsers = data
         .map((user) => ({
